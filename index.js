fd63e392b73d9e6079d71b4200eda6ee16ca95ed
Aly <aly.kazani52@gmail.com>
2026-07-20 22:57:07 +0500

    Gridified the page a bit, and some other stuff

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 console.log('Is this working?');
+
+function updateClock() {
+    const now = new Date();
+    const time = now.toLocaleTimeString('en-US', { hour12: true});
+
+    const element = document.getElementById('time');
+
+    if (element) {
+        element.textContent = time;
+    }
+}
+
+updateClock();
+setInterval(updateClock, 1000);
