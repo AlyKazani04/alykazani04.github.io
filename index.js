65f124a2ae967146252d5d61ab2ff8f7e6ae99c5
Aly <aly.kazani52@gmail.com>
2026-08-04 16:44:57 +0500

    Projects Card Style Personalization

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 console.log('Is this working?');
+
+const modal = document.getElementById("image-modal");
+const modalImg = document.getElementById("modal-content");
+const closeBtn = document.getElementsByClassName("close")[0];
+
+const images = document.querySelectorAll(".project-img");
+
+images.forEach((img) => {
+  img.onclick = function() {
+    modal.style.display = "block";
+    modalImg.src = this.src;
+  }
+});
+
+closeBtn.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target === modal) {
+    modal.style.display = "none";
+  }
+}
